b9b9fb8ac7d9f1f3cf64332dfb4da90da8fb4013
Alonso Martinez Cisneros <martinez@zib.de>
2024-07-18 16:10:35 +0200

    Adding influencer switching callback

--- to_diffeq_test.jl
+++ to_diffeq_test.jl
@@ -11,8 +11,18 @@ u₀ = vcat(omp.X, omp.I, omp.M)
 
 # Simple SDE problem
 problem = SDEProblem(drift, noise, u₀, (0.0, 2.0), P)
-ssol = solve(problem, SRIW1())
+# Introducing the callback
+condition(u, t, integrator) = true
+function affect!(integrator)
+    dt = get_proposed_dt(integrator)
+    res = influencer_callback(integrator.u, integrator.p, dt)
+    @show res
+    integrator.p.C .= influencer_callback(integrator.u, integrator.p, dt)
+end
+cb = DiscreteCallback(condition, affect!)
 
-# Ensemble simulations
-ensembleprob = EnsembleProblem(problem)
-esol = solve(ensembleprob, SRIW1(), EnsembleThreads(); trajectories=100)
+ssol = solve(problem, SRIW1(), callback = cb, seed = seed)
+
+# # Ensemble simulations
+# ensembleprob = EnsembleProblem(problem)
+# esol = solve(ensembleprob, SRIW1(), EnsembleThreads(); trajectories=100)
