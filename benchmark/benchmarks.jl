d2c77f9cdefa8def98fc9b036aaf2d54e2956bdf
Alonso Martínez Cisneros <alonso.mtz99@gmail.com>
2024-07-25 01:56:40 +0200

    Take advantage of destructuring ModelProblem in benchmark script

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,7 +6,8 @@ SUITE["abm_simulation"] = BenchmarkGroup()
 
 omp = OpinionModelProblem((-2.0, 2.0), (-2.0, 2.0))
 prob = OpinionDynamicsABM.build_sdeproblem(omp, (0.0, 2.0))
-X, M, I, A, Z, Y = OpinionDynamicsABM.get_values(omp)
+# X, M, I, A, Z, Y = OpinionDynamicsABM.get_values(omp)
+X, M, I, A, Z, Y = omp
 SUITE["abm_simulation"]["agent-agent attraction"] = @benchmarkable(
     AgAg_attraction($X, $A)
 )
