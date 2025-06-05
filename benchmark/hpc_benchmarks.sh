b56fa75ab0e006236f9bd24b1a96803730d1537b
Alonso Martinez Cisneros <martinez@zib.de>
2025-06-05 17:01:20 +0200

    Create hpc_benchmarks.sh

---
+++ benchmark/hpc_benchmarks.sh
@@ -0,0 +1,28 @@
+#!/bin/zsh
+#SBATCH --job-name=benchmarks_abm     # create a short name for your job
+#SBATCH --chdir=/home/htc/amartine/OpinionDynamicsABM.jl
+#SBATCH --output=out/slurm-%A.%a.out # stdout file
+#SBATCH --error=out/err/slurm-%A.%a.err  # stderr file
+#SBATCH --nodes=1                # node count
+#SBATCH --ntasks=32               # total number of tasks across all nodes
+#SBATCH --cpus-per-task=2        # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
+#SBATCH --time=2:50:50          # total run time limit (HH:MM:SS)
+#SBATCH --mail-user=martinez@zib.de
+#SBATCH --mail-type=all          # send email on job start, end and fault
+
+echo "My SLURM_ARRAY_JOB_ID is $SLURM_ARRAY_JOB_ID."
+echo "My SLURM_ARRAY_TASK_ID is $SLURM_ARRAY_TASK_ID"
+echo "Executing on the machine:" $(hostname)
+echo "\n\n\n"
+
+/scratch/htc/amartine/julia/bin/benchpkg \
+    -r $(git log --pretty=format:'%h' -n 7 | tr '\n' ',') \
+    -o tmp/benchresults \
+    --exeflags="--threads auto -O3" \
+    --tune
+
+/scratch/htc/amartine/julia/bin/benchpkgplot \
+    -r $(git log --pretty=format:'%h' -n 7 | tr '\n' ',') \
+    -i tmp/benchresults \
+    -o tmp/ \
