3490058b5cb5893894018642df7256367ac44150
Alonso Martínez Cisneros <alonso.mtz99@gmail.com>
2025-06-07 15:40:08 +0200

    Added benchmarks for influencer switch rates

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -6,12 +6,26 @@ SUITE["abm_simulation"] = BenchmarkGroup()
 
 omp = OpinionModelProblem((-2.0, 2.0), (-2.0, 2.0))
 prob = OpinionDynamicsABM.build_sdeproblem(omp, (0.0, 2.0))
-# X, M, I, A, Z, Y = OpinionDynamicsABM.get_values(omp)
-# X, M, I, A, Z, Y = omp
-SUITE["abm_simulation"]["agent-agent attraction"] = @benchmarkable AgAg_attraction(X, A) setup = (X=copy(omp.X);
-                                                                                                  A=copy(omp.A))
 
-SUITE["abm_simulation"]["full simulation"] = @benchmarkable simulate!(abm) setup = (abm = deepcopy(omp))
+SUITE["abm_simulation"]["agent-agent attraction"] = @benchmarkable begin
+    setup = (X = copy(omp.X); A = copy(omp.A))
+    AgAg_attraction(X, A)
+end
+
+SUITE["amb_simulation"]["influencer switch rates"] = @benchmarkable begin
+    setup = (Ril = similar(omp.X, omp.p.n, omp.p.L);
+             X = copy(omp.X);
+             Z = copy(omp.Z);
+             B = copy(omp.B);
+             C = copy(omp.C);
+             η = omp.p.η)
+    OpinionDynamicsABM.influencer_switch_rates!(Ril, X, Z, B, C, η)
+end
+
+SUITE["abm_simulation"]["full simulation"] = @benchmarkable begin
+    setup = (abm = deepcopy(omp))
+    simulate!(abm)
+end
 
 # SUITE["abm_simulation"]["diffeq_integrator"] = @benchmarkable(
 #     simulate!($prob)
