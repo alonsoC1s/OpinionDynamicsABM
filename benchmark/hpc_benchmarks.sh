a8cd8e6549070389d65f890c1143797d10fad708
Alonso Martinez Cisneros <martinez@zib.de>
2025-06-10 10:56:35 +0200

    Benchmark using tags

--- benchmark/hpc_benchmarks.sh
+++ benchmark/hpc_benchmarks.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=out/slurm-%A.%a.out # stdout file
 #SBATCH --error=out/err/slurm-%A.%a.err  # stderr file
 #SBATCH --nodes=1                # node count
-#SBATCH --ntasks=32               # total number of tasks across all nodes
+#SBATCH --ntasks=1               # total number of tasks across all nodes
 #SBATCH --cpus-per-task=2        # cpu-cores per task (>1 if multi-threaded tasks)
 #SBATCH --mem-per-cpu=4G         # memory per cpu-core (4G is default)
 #SBATCH --time=2:50:50          # total run time limit (HH:MM:SS)
@@ -15,14 +15,15 @@ echo "My SLURM_ARRAY_JOB_ID is $SLURM_ARRAY_JOB_ID."
 echo "My SLURM_ARRAY_TASK_ID is $SLURM_ARRAY_TASK_ID"
 echo "Executing on the machine:" $(hostname)
 echo "\n\n\n"
+# -r $(git log --pretty=format:'%h' -n 7 | tr '\n' ',') \
 
 /scratch/htc/amartine/julia/bin/benchpkg \
-    -r $(git log --pretty=format:'%h' -n 7 | tr '\n' ',') \
+    -r vUseFill,vReuseBuffers,vTmpBuffer,v@inbounds,vNoNorm,vNoReturn,vExpFast \
     -o tmp/benchresults \
     --exeflags="--threads auto -O3" \
     --tune
 
-/scratch/htc/amartine/julia/bin/benchpkgplot \
-    -r $(git log --pretty=format:'%h' -n 7 | tr '\n' ',') \
+/scratch/htc/amartine/julia/bin/benchpkgplot OpinionDynamicsABM \
+    -r vUseFill,vReuseBuffers,vTmpBuffer,v@inbounds,vNoNorm,vNoReturn,vExpFast \
     -i tmp/benchresults \
     -o tmp/ \
